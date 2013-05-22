c316c78d63b127fe470a6304410c6cef760b2bf7
Nikolay Kolev <nikolay.kolev@sirma.bg>
2013-05-22 11:56:00 +0300

    more bugs

--- Reflection, Annotations, RegEx/src/main/java/com/sirma/itt/javacourse/reflections/RandomModificatorsClass.java
+++ Reflection, Annotations, RegEx/src/main/java/com/sirma/itt/javacourse/reflections/RandomModificatorsClass.java
@@ -10,6 +10,7 @@ public class RandomModificatorsClass {
 	public int publicInt = 1;
 
 	/** The private float. */
+	@SuppressWarnings("unused")
 	private float privateFloat;
 
 	/** The protected string. */
@@ -30,6 +31,7 @@ public class RandomModificatorsClass {
 	 * 
 	 * @return the double
 	 */
+	@SuppressWarnings("unused")
 	private double privateDouble() {
 		return 1.1;
 
